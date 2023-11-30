1cc2424ab272cce478c4e9c1282be0486927ae7e
Itamara Ferreira <itamaraaferreira100@gmail.com>
2023-11-30 15:53:01 -0300

    Criando AbTag

--- example/index.tsx
+++ example/index.tsx
@@ -1,15 +1,16 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { AbBotao, AbCard } from '../.';
+import { AbBotao, AbCard, AbTag } from '../.';
 
 const App = () => {
   return (
     <div>
-      <AbBotao />
-      <AbCard></AbCard>   
-    </div> 
+      <AbBotao /> 
+      <AbCard>ihuuuul</AbCard>   
+      <AbTag />
+    </div>  
   );
-};
+}; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
